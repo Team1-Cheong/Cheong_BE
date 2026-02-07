345a086bd8f967a0bab751864f83d79e50fdd331
오영훈 <skek2412@naver.com>
2026-02-08 06:22:22 +0900

    feat: add cors domain  #32

--- src/main/java/com/springdemo/main/cheong_be/config/WebConfig.java
+++ src/main/java/com/springdemo/main/cheong_be/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**") // 모든 경로에 대해
-        .allowedOrigins("http://localhost:3000") // Next.js의 포트 허용
+        .allowedOrigins("http://localhost:3000","https://cheong-fe.vercel.app") // Next.js의 포트 허용
         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 HTTP 메서드
         .allowedHeaders("*") // 모든 헤더 허용
         .allowCredentials(true) // 쿠키/인증 정보 포함 허용 (로그인 구현 시 필수)
