e5585cedf3cd487ea7cf3012bea9281240e3428c
Mingyu Lee <mgl327@cau.ac.kr>
2026-02-07 18:22:34 +0900

    Initial commit

---
+++ src/main/java/com/springdemo/test/cheong_be/CheongBeApplication.java
@@ -0,0 +1,13 @@
+package com.springdemo.test.cheong_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheongBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheongBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springdemo/test/cheong_be/CheongBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdemo.test.cheong_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheongBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
